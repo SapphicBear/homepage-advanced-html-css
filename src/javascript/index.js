97509d542aae2b28c6302d39363f94bf906da6cf
SapphicBear <sleepyscreenart@gmail.com>
2026-02-16 17:55:51 +0100

    add: svg for share and random colors

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -1,1 +1,15 @@
 import "../styles.css";
+
+(function(){
+    const projects = document.querySelectorAll("section.card");
+    const colors = ["#F54927", "#E8BC4A", "#6DC7B9", "#A06DC7", "#C76D9E"];
+
+    const randomColors = () => {
+        projects.forEach((card) => {
+            const randomNumber = Math.floor(Math.random() * colors.length);
+            card.style.backgroundColor = colors[randomNumber];
+        });
+    };
+
+    randomColors();
+})();
